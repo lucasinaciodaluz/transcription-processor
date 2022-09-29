f2522066af97b7e0e6840f56296334318e3de998
Lucas Luz <lawlithi@gmail.com>
2022-09-29 02:07:42 -0300

    Deploy app to Heroku

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,15 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
