b6431a02a65b3cf7956e3a790bb9ab6e7408d6b3
Lucas Luz <lawlithi@gmail.com>
2022-09-29 11:56:38 -0300

    make it better

--- server/index.ts
+++ server/index.ts
@@ -13,3 +13,7 @@ app.get("/api", (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
